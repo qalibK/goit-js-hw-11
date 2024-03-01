3e96f21e961f8975d7d3d0a4845127cd9b2487ec
qalibK <bvomusak01@gmail.com>
2024-03-02 00:50:44 +0100

    rollup config changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import { copy } from 'vite-plugin-copy';
 
 export default defineConfig(({ command }) => {
   return {
@@ -11,21 +12,18 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync('./src/.html'),
         output: {
           assetFileNames: ({ name }) => {
-            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            if (/.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
               return 'assets/images/[name]-[hash][extname]';
             }
-
-            if (/\.css$/.test(name ?? '')) {
+            if (/.css$/.test(name ?? '')) {
               return 'assets/css/[name]-[hash][extname]';
             }
             return 'assets/[name]-[hash][extname]';
           },
-
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
@@ -36,6 +34,16 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/.html']),
+      copy({
+        targets: [
+          { src: 'src/img/images/*', dest: 'dist/img/images' },
+          // Add more targets if you have other asset types
+        ],
+        verbose: true, // Optional. Logs the copied files
+      }),
+    ],
   };
 });
