44e26d582e23bc507b82bc917a15065c8cb57e52
qalibK <bvomusak01@gmail.com>
2024-03-02 00:26:24 +0100

    assets

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -57,7 +57,7 @@ export function onError(error) {
     maxWidth: 432,
     maxHeigth: 88,
     animateInside: true,
-    iconUrl: './css/x-octagon.svg',
+    iconUrl: './assets/x-octagon.svg',
     transitionIn: 'fadeInRight',
     transitionOut: 'fadeOutRight',
   });
